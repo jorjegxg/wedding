74358c350878b8004c88786f52af06f50423e8ab
Gheorghe Luta <yotrevorgxg@gmail.com>
2025-10-20 12:17:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('btn');
+const output = document.getElementById('output');
+
+button.addEventListener('click', () => {
+  output.textContent = 'Buton apăsat! Si inca ceva 🔥';
+});
+
